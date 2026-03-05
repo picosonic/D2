0c79aa52ef0c136692e706182558a64b6c336115
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2026-03-05 13:41:22 +0000

    Added binary file comparison

---
+++ bincompare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+# Compare 2x binary files
+
+if [ $# -lt 2 ]
+then
+  echo "Specify two files to compare"
+  exit 0
+fi
+
+bina="$1"
+binb="$2"
+
+hexdump -v -C ${bina} > ${bina}.bincomp
+hexdump -v -C ${binb} > ${binb}.bincomp
+
+meld ${bina}.bincomp ${binb}.bincomp 2>&1
+rm ${bina}.bincomp ${binb}.bincomp >/dev/null 2>&1
